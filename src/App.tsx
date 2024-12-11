987439266336df644657cb8f43931fa471e8dbc5
zvivaa <iazl1337666@gmail.com>
2024-12-11 21:00:10 +0500

    #add css-module support

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import News from './pages/News'
-import NewsDetails from './pages/NewsDetails'
-import Favorites from './pages/Favorites'
-import styles from './styles/App.module.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import News from './pages/News';
+import NewsDetails from './pages/NewsDetails';
+import Favorites from './pages/Favorites';
+import styles from './styles/app.module.css';
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <div className={styles.app}>
-        <Routes>
-          <Route path="/" element={<News />} />
-          <Route path="/news/:id" element={<NewsDetails />} />
-          <Route path="/favorites" element={<Favorites />} />{' '}
-        </Routes>
-      </div>
-    </Router>
-  )
-}
+    return (
+        <Router>
+            <div className={styles.app}>
+                <Routes>
+                    <Route path="/" element={<News />} />
+                    <Route path="/news/:id" element={<NewsDetails />} />
+                    <Route path="/favorites" element={<Favorites />} />{' '}
+                </Routes>
+            </div>
+        </Router>
+    );
+};
 
-export default App
+export default App;
